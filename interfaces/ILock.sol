348adfa738446d69163dd4bca81e46740c502263
OwlOfMoistness <owl@yieldguild.games>
2022-03-27 13:22:43 +0200

    Style

--- interfaces/ILock.sol
+++ interfaces/ILock.sol
@@ -2,8 +2,6 @@ pragma solidity ^0.8.2;
 
 interface ILock {
 	function lockId(uint256 _id) external;
-
 	function unlockId(uint256 _id) external;
-
 	function freeId(uint256 _id, address _contract) external;
 }
