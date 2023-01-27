27088517e0d523c865abd11981be364463989a0a
OwlOfMoistness <owl@yieldguild.games>
2023-01-27 19:36:08 +0100

    Rename file

--- contracts/TestNft.sol
+++ contracts/TestNft.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.2;
 
-import "./Erc721LockRegistry.sol";
+import "./ERC721x.sol";
 
 contract TestNFT is ERC721x("", "") {
 	function mint(uint256 _id) external {
